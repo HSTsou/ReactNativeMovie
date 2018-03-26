31c783483c49514b4986417c8fcdeb3927352ee3
hs <hs@hsde-MacBook-Pro.local>
2018-03-26 15:03:23 +0800

    [what]modify the eslint sytle

--- src/Work.js
+++ src/Work.js
@@ -1,119 +1,67 @@
-import React, { Component } from "react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React, { Component } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     paddingTop: 60,
-    alignItems: "center",
-    flex: 1
+    alignItems: 'center',
+    flex: 1,
   },
   button: {
     marginBottom: 30,
-    alignSelf: "stretch",
-    alignItems: "center",
-    backgroundColor: "#2196F3"
+    alignSelf: 'stretch',
+    alignItems: 'center',
+    backgroundColor: '#2196F3',
   },
   buttonText: {
-    padding: 20,
-    color: "white",
-    alignItems: "center"
+    padding: 10,
+    color: 'white',
+    alignItems: 'center',
   },
   counterRow: {
     padding: 10,
-    color: "white",
-    flexDirection: "row",
-    flex: 1
+    flexDirection: 'row',
+    flex: 1,
   },
   counterLeftView: {
-    alignItems: "center",
-    margin: 8,
-    color: "white",
-    flex: 0.4
+    flex: 0.4,
   },
   counterRightView: {
-    alignItems: "center",
-    margin: 8,
-    color: "white",
-    flex: 0.6
+    flex: 0.6,
   },
   textBox: {
-    alignItems: "center",
-    alignSelf: "stretch",
+    alignItems: 'center',
+    alignSelf: 'stretch',
+    justifyContent: 'center',
     margin: 8,
-    backgroundColor: "#2196F3",
-    height: 100
-  }
+    backgroundColor: '#2196F3',
+    height: 100,
+  },
 });
 
+const resultStr = {
+  three: 'A',
+  five: 'B',
+  threeAndFive: 'AB',
+  notThreeOrFive: 'not A or B',
+};
+
 let timer;
+let index = 0;
 
 export default class Work extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: [],
-      value: "",
-      currentNumber: "",
-      index: 0
+      numberArray: [],
+      result: '',
+      currentNumber: '',
     };
   }
 
   componentDidMount() {
-    console.info("componentDidMount");
-    this.setNumber();
+    this.initNumbers();
   }
-
-  onPressButton = () => {
-    // var c = this.state.count;
-  };
-
-  setNumber = () => {
-    let number = 1;
-    const countsArray = [];
-    // console.info("countsArray");
-    for (let index = 0; index < 100; index++) {
-      // console.info(number);
-      countsArray[index] = number++;
-    }
-
-    this.setState({
-      count: countsArray
-    });
-  };
-
-  printValue = number => {
-    if (this.isThree(number) && this.isFive(number)) {
-      this.setState({
-        value: "AB"
-      });
-    } else if (this.isThree(number)) {
-      this.setState({
-        value: "A"
-      });
-    } else if (this.isFive(number)) {
-      this.setState({
-        value: "B"
-      });
-    } else {
-      this.setState({
-        value: "not A or B"
-      });
-    }
-  };
-
-  isThree = number => {
-    if (number % 3 === 0) {
-      return true;
-    }
-    return false;
-  };
-
-  isFive = number => {
-    if (number % 5 === 0) {
-      return true;
-    }
-    return false;
-  };
 
   onStartCount = () => {
     timer = setInterval(this.timer, 1000);
@@ -123,31 +71,72 @@ export default class Work extends Component {
     clearInterval(timer);
   };
 
-  timer = () => {
-    const c = this.state.count;
-    const i = this.state.index;
-    this.printValue(c[i]);
-    console.info(c[i]);
+  initNumbers = () => {
+    let number = 1;
+    const countsArray = [];
+    for (let i = 0; i < 100; i++) {
+      countsArray[i] = number++;
+    }
+
     this.setState({
-      currentNumber: c[i]
+      numberArray: countsArray,
+    });
+  };
+
+  printValue = (number) => {
+    if (this.isThree(number) && this.isFive(number)) {
+      this.setState({
+        result: resultStr.threeAndFive,
+      });
+    } else if (this.isThree(number)) {
+      this.setState({
+        result: resultStr.three,
+      });
+    } else if (this.isFive(number)) {
+      this.setState({
+        result: resultStr.five,
+      });
+    } else {
+      this.setState({
+        result: resultStr.notThreeOrFive,
+      });
+    }
+  };
+
+  isThree = (number) => {
+    if (number % 3 === 0) {
+      return true;
+    }
+    return false;
+  };
+
+  isFive = (number) => {
+    if (number % 5 === 0) {
+      return true;
+    }
+    return false;
+  };
+
+  timer = () => {
+    const numberArray = this.state.numberArray;
+    this.printValue(numberArray[index]);
+    console.info(numberArray[index]);
+    this.setState({
+      currentNumber: numberArray[index],
     });
 
-    if (i < 100) {
-      this.setState({ index: this.state.index + 1 });
-      console.info("index = ", this.state.index);
+    if (index < 100) {
+      index++;
+      console.info('index = ', index);
     } else {
-      console.info("i > = 100");
-      console.info("index = ", this.state.index);
+      console.info('i > = 100');
     }
   };
 
   render() {
     return (
       <View style={styles.container}>
-        <TouchableOpacity
-          style={styles.counterView}
-          onPress={this.onStartCount}
-        >
+        <TouchableOpacity style={styles.counterView} onPress={this.onStartCount}>
           <View style={styles.button}>
             <Text style={styles.buttonText}>START</Text>
           </View>
@@ -155,25 +144,22 @@ export default class Work extends Component {
 
         <TouchableOpacity style={styles.counterView} onPress={this.onStopTimer}>
           <View style={styles.button}>
-            <Text style={styles.buttonText}>STOP</Text>
+            <Text style={styles.buttonText}>PAUSE</Text>
           </View>
         </TouchableOpacity>
 
         <View style={styles.counterRow}>
-          <TouchableOpacity
-            style={styles.counterLeftView}
-            onPress={this.onPressButton}
-          >
+          <View style={styles.counterLeftView}>
             <View style={styles.textBox}>
               <Text style={styles.buttonText}>{this.state.currentNumber}</Text>
             </View>
-          </TouchableOpacity>
+          </View>
 
-          <TouchableOpacity style={styles.counterRightView}>
+          <View style={styles.counterRightView}>
             <View style={styles.textBox}>
-              <Text style={styles.buttonText}>{this.state.value}</Text>
+              <Text style={styles.buttonText}>{this.state.result}</Text>
             </View>
-          </TouchableOpacity>
+          </View>
         </View>
       </View>
     );
